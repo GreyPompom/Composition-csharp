77ed3b50f86084cf0e0cc1ea8953a9331b98f8bd
Emely - Desenvolvimento 06 <desenvolvimento06@pauta.com.br>
2024-03-18 16:50:20 -0300

    initial implement

---
+++ Course/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities.Enums
+{
+    enum EnumWorkerLevel : int
+    {
+        Junior = 0,
+        Mid_level = 1,
+        Senior = 2
+    }
+}
